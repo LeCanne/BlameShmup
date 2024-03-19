6c016772c8bbfa26e0017be7615f7e725f792651
Melvin Le Canne <94301906+LeCanne@users.noreply.github.com>
2024-03-19 17:03:51 +0100

    ElectricBarrier(Function complete)

--- Assets/Scripts/EnemyAI/ElecticBarrier.cs
+++ Assets/Scripts/EnemyAI/ElecticBarrier.cs
@@ -6,16 +6,19 @@ using UnityEngine;
 public class ElecticBarrier : MonoBehaviour
 {
     public GameObject electricBarrier;
+    public LayerMask layer;
     public float timer;
     public float speed;
     public int randomDir;
     private bool checkWall;
     private float randomTime;
+    public bool vulnerable;
+    public int health;
     // Start is called before the first frame update
     void Start()
     {
         randomDir = Random.Range(1, 3);
-        randomTime = Random.Range(1f, 4f);
+        randomTime = Random.Range(1f, 3f);
     }
 
     // Update is called once per frame
@@ -24,6 +27,7 @@ public class ElecticBarrier : MonoBehaviour
         timer += Time.deltaTime;
 
         Move();
+        CheckHP();
     }
 
     public void Move()
@@ -50,18 +54,35 @@ public class ElecticBarrier : MonoBehaviour
            
            
         }
-        
-        if (Physics2D.Raycast(transform.position, transform.right, 1f))
+
+        if (Physics2D.Raycast(transform.position, transform.right, 1f, layer) || Physics2D.Raycast(transform.position, -transform.right, 1f, layer))
         {
             
             checkWall = true;
             electricBarrier.SetActive(true);
+            vulnerable = true;
         }
 
-        if (Physics2D.Raycast(transform.position, -transform.right, 1f))
+        
+    }
+
+    public void CheckHP()
+    {
+        if(health <= 0)
         {
-            checkWall = true;
-            electricBarrier.SetActive(true);
+            Destroy(gameObject);
         }
+    }
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        if(vulnerable == true)
+        {
+            if (other.gameObject.tag == "PlayerBullet")
+            {
+                health--;
+                Destroy(other.gameObject);
+            }
+        }
+       
     }
 }
