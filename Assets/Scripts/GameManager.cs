c42bc452bb27f3b4bcd4155143ecda91a77b9c4f
Melvin Le Canne <94301906+LeCanne@users.noreply.github.com>
2024-03-21 15:07:51 +0100

    TitleGold

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+   public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void NextScene(int sceneChosen)
+    {
+        SceneManager.LoadScene(sceneChosen);
+    }
+}
